cb90b18c920824eaf5f49accbe31c33645ee54bb
Arsen <arsjj7@gmail.com>
2024-01-06 15:35:26 +0400

    first setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="border border-blue-500 p-4 rounded-lg">Vite + React</h1>
+      <Button color="primary">Click</Button>
+    </>
+  );
+}
+
+export default App;
